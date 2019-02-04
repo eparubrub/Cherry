4bf6fc709d38828d9ca118d1fabbf77639a4cc3b
Emmit Parubrub <emmit.parubrub@gmail.com>
2019-02-04 09:46:02 -0800

    Add files via upload

--- restock.py
+++ restock.py
@@ -1,28 +1,20 @@
 __author__ = 'Emmit'
 import requests
-import urllib3
 from bs4 import BeautifulSoup
-import webbrowser
 import time
-import smtplib
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome()
-browser.get('')
+#enter url below
+url = ""
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
 
 while True:
-    url = ""
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #edit accordingly
-    name_box = soup.find('b', attrs={'class': 'button sold-out'})
-    string_button = str(name_box)
-    string_none = "None"
-    if string_button == string_none:
-        print('UPDATED')
-        webbrowser.open(url)
-        break
+    page2 = requests.get(url)
+    soup2 = BeautifulSoup(page.content, 'html.parser')
+    if soup != soup2:
+        while True:
+            print("UPDATED")
+        soup = soup2
     else:
-        time.sleep(60)
+        time.sleep(5)
         continue
